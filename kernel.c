293f23884ba343b5232a08202ca621fa477cb4fe
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-06-27 14:54:35 +0200

    Implement newline characters in the terminal

--- kernel.c
+++ kernel.c
@@ -85,6 +85,12 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c)
 {
+    if (c == '\n') {
+        terminal_column = 0;
+        terminal_row++;
+        return;
+    }
+
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH) {
         terminal_column = 0;
@@ -108,5 +114,5 @@ void kernel_main(void)
 {
     /* Initialize terminal interface */
     terminal_initialize();
-    terminal_writestring("CarrotOS booted!\n");
+    terminal_writestring("CarrotOS booted!\nThis is a newline test!");
 }
