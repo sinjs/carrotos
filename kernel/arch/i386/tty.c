3f8e6b1d9fdd042b5c4b7da5d1e5ef5c23c8193f
sinjs <8802785-sinjs@users.noreply.gitlab.com>
2021-06-28 22:42:15 +0200

    Kernel/TTY: Add newline support

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -40,6 +40,11 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
+	if (c == '\n') {
+		terminal_column = 0;
+		terminal_row++;
+		return;
+	}
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
